c1010acadfaffec1a28c81b124fdd50081b31c9e
mestrovicd <dariomestro@gmail.com>
2023-06-23 21:42:58 +0200

    added playername validation

--- tic-tac-toe/scripts/app.js
+++ tic-tac-toe/scripts/app.js
@@ -1,5 +1,7 @@
 const playerConfigOverlay = document.getElementById("config-overlay");
 const backdrop = document.getElementById("backdrop");
+const form = document.querySelector("form");
+const errorsOutput = document.getElementById("config-error");
 
 const editPlayerOneBtn = document.getElementById("edit-p1");
 const editPlayerTwoBtn = document.getElementById("edit-p2");
@@ -10,3 +12,5 @@ editPlayerTwoBtn.addEventListener("click", openPlayerConfig);
 
 cancelConfigBtn.addEventListener("click", closePlayerConfig);
 backdrop.addEventListener("click", closePlayerConfig);
+
+form.addEventListener("submit", savePlayerConfig);
--- tic-tac-toe/scripts/config.js
+++ tic-tac-toe/scripts/config.js
@@ -1,4 +1,5 @@
-function openPlayerConfig() {
+function openPlayerConfig(e) {
+    const clickedButton = e.target;
     playerConfigOverlay.style.display = "block";
     backdrop.style.display = "block";
 }
@@ -6,4 +7,18 @@ function openPlayerConfig() {
 function closePlayerConfig() {
     playerConfigOverlay.style.display = "none";
     backdrop.style.display = "none";
+    form.firstElementChild.classList.remove("error");
+    errorsOutput.textContent = "";
+}
+
+function savePlayerConfig(e) {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const enteredPlayerName = formData.get("playername").trim();
+
+    if (!enteredPlayerName) {
+        e.target.firstElementChild.classList.add("error");
+        errorsOutput.textContent = "Please enter a valid name";
+        return;
+    }
 }
